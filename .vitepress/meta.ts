edb100e65941b3873d930fea580241bacaeb4a0a
pileax <ipileax@gmail.com>
2025-02-27 08:56:21 +0800

    refactor: 💡 meta

--- .vitepress/meta.ts
+++ .vitepress/meta.ts
@@ -3,4 +3,4 @@ export const keywords = 'vitepress, i18n, template';
 export const description = 'VitePress i18n template';
 export const descriptionZh = 'VitePress 国际化模板';
 export const site = 'https://guide.pileax.ai';
-export const repo = 'https://github.com/pileax/pileax-ai-guide';
+export const repo = 'https://github.com/pileax-ai/pileax-ai-guide';
